0484cd8516fd1582493f8e516cf4536e0f74e326
dimo <omidjalilian147@gmail.com>
2022-05-16 22:19:30 +0430

    android 12 support

--- android/app/src/main/kotlin/com/dimo/gtuner/MainActivity.kt
+++ android/app/src/main/kotlin/com/dimo/gtuner/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.dimo.gtuner.gtuner
+package com.dimo.gtuner
 
 import io.flutter.embedding.android.FlutterActivity
 
--- android/app/src/main/kotlin/com/example/gtuner/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.gtuner
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
